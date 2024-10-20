cb3a180dd4cea7cb8398a475c856c5855c5caa84
lsmmega <pogmegalsm@outlook.com>
2024-10-20 08:20:43 +0800

    create 1up

--- build.bat
+++ build.bat
@@ -4,6 +4,7 @@ bmp2nes gfx/obj/capsule/large_life_capsule.bmp
 bmp2nes gfx/obj/capsule/small_life_capsule.bmp
 bmp2nes gfx/obj/capsule/large_wpn_capsule.bmp
 bmp2nes gfx/obj/capsule/small_wpn_capsule.bmp
+bmp2nes gfx/obj/1up.bmp
 bmp2nes gfx/obj/killed.bmp
 bmp2nes gfx/obj/bar.bmp
 asm6 main.asm rm5.nes
